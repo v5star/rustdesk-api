befd90347ed01ef1d366d581b22f48a45d375b93
v5star <31580191+v5star@users.noreply.github.com>
2024-02-05 18:15:16 +0800

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p /run/php
-chown -R www-data:www-data /run/php
-chown -R www-data:www-data /var/www/data
-rm -rf /var/www/html/index.nginx-debian.html
-php-fpm -D 
-
-exec "$@"
